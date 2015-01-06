7c3c82c112cf8a48b0e356f105f32d246ca1d5b8
Will Vaughn <willieviseoae@gmail.com>
2015-01-06 15:18:55 -0800

    reformat gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ var istanbul = require('gulp-istanbul');
 var mocha = require('gulp-mocha'); // jshint ignore:line
 
 gulp.task('test', function (cb) {
-    gulp.src(['lib/**/*.js', 'index.js'])
-        .pipe(istanbul())
-        .pipe(istanbul.hookRequire())
-        .on('finish', function () {
-            gulp.src(['test/**/*.js'])
-                .pipe(mocha())
-                .pipe(istanbul.writeReports())
-                .on('end', cb);
-        });
+  gulp.src(['lib/**/*.js', 'index.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      gulp.src(['test/**/*.js'])
+        .pipe(mocha())
+        .pipe(istanbul.writeReports())
+        .on('end', cb);
+    });
 });
