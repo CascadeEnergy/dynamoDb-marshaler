66e75ff2f36680ab3fc71152ae6cb8d217f97d5b
Will Vaughn <willieviseoae@gmail.com>
2015-02-22 12:02:03 -0800

    A little unit test cleanup while I'm in here. :)

--- test/commands/unmarshalList.js
+++ test/commands/unmarshalList.js
@@ -1,21 +1,39 @@
 'use strict';
 
+var sinon = require('sinon');
+var should = require('should');
 var unmarshalList = require('../../lib/commands/unmarshalList');
-var unmarshal = function(item) {
-  return item + ' unmarshaled';
-};
 
 describe('unmarshalList', function() {
+  var unmarshal;
+
+  beforeEach(function() {
+    unmarshal = sinon.stub();
+  });
+
   it('should return undefined if the item does not have "L" key', function() {
     var result = unmarshalList({S: 'foo'}, unmarshal);
+    unmarshal.callCount.should.equal(0);
     (result === undefined).should.equal(true);
   });
 
   it(
     'should unmarshal the list, and unmarshal each value in the list',
     function() {
-      unmarshalList({L: ['FOOOBJ', 'BAROBJ']}, unmarshal)
-        .should
-        .eql(['FOOOBJ unmarshaled', 'BAROBJ unmarshaled']);
+      var result;
+
+      unmarshal.onFirstCall().returns('foo unmarshaled');
+      unmarshal.onSecondCall().returns('bar unmarshaled');
+
+      result = unmarshalList({L: ['foo', 'bar']}, unmarshal);
+
+      unmarshal.calledTwice.should.equal(true);
+      unmarshal.args[0][0].should.eql('foo');
+      unmarshal.args[1][0].should.eql('bar');
+
+      result.should.eql([
+        'foo unmarshaled',
+        'bar unmarshaled'
+      ]);
     });
 });
